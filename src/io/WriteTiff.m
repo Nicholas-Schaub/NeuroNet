51e7230feffe6c23f334bcc31f3304a35bb28da6
Nick Schaub <nicholas.j.schaub@gmail.com>
2019-06-05 21:39:28 -0400

    Updated Tiff commands to fix flipping bug.

--- src/io/WriteTiff.m
+++ src/io/WriteTiff.m
@@ -1,8 +1,12 @@
-function WriteTiff(S,image_path)
+function WriteTiff(S,image_path,add_tags)
 
     t = Tiff(image_path,'w');
     
     switch(class(S))
+        case 'logical'
+            S = uint8(S);
+            sFormat = Tiff.SampleFormat.UInt;
+            bps = 8;
         case 'uint8'
             sFormat = Tiff.SampleFormat.UInt;
             bps = 8;
@@ -34,15 +38,36 @@ function WriteTiff(S,image_path)
             sFormat = Tiff.SampleFormat.IEEEFP;
             bps = 64;
     end
-    t.setTag('Photometric',Tiff.Photometric.MinIsBlack);
-    t.setTag('Compression',Tiff.Compression.AdobeDeflate);
-    t.setTag('BitsPerSample',bps);
-    t.setTag('SamplesPerPixel',size(S,3));
-    t.setTag('SampleFormat',sFormat);
-    t.setTag('ImageLength',size(S,1));
-    t.setTag('ImageWidth',size(S,2));
-    t.setTag('PlanarConfiguration',Tiff.PlanarConfiguration.Chunky);
-
-    t.write(S);
+    
+    % Setup the tiff file for export
+    if numel(S)<2^32-1
+        t = Tiff(image_path,'w');
+    else
+        t = Tiff(image_path,'w8');
+    end
+    tags.Photometric = Tiff.Photometric.MinIsBlack;
+    tags.Compression = Tiff.Compression.AdobeDeflate;
+    tags.BitsPerSample = bps;
+    tags.SamplesPerPixel = size(S,3);
+    tags.SampleFormat = sFormat;
+    tags.ImageLength = size(S,1);
+    tags.ImageWidth = size(S,2);
+    tags.PlanarConfiguration = Tiff.PlanarConfiguration.Chunky;
+    
+    if nargin>2
+        fields = fieldnames(add_tags);
+        for j = fields'
+            tags.(j{1}) = add_tags.(j{1});
+        end
+    end
+    
+    for j=1:size(S,4)
+        t.setTag(tags);
+        t.write(S(:,:,:,j));
+        if j~=size(S,4)
+            t.writeDirectory();
+        end
+    end
+    
     t.close();
 end
